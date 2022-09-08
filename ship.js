8c1c71a45cbcfdd1385289ac20a8b2c42ddb773c
Josh S <aaabccc87@protonmail.com>
2022-09-08 18:24:34 -0400

    Constructor for Ship and methods for hit and isSunk added

---
+++ ship.js
@@ -0,0 +1,32 @@
+const shipProto = {
+  hit(index) {
+    // if index is within bounds of damage array, return true
+    if (index >= 0 && index < this.length) {
+      this.damage[index] = 1;
+      return true;
+    }
+
+    return false;
+  },
+  isSunk() {
+    // val of 1 signifies a hit
+    return this.damage.every((val) => val === 1);
+  },
+};
+
+function Ship(type, length) {
+  const newShip = Object.create(shipProto);
+
+  newShip.type = type;
+  newShip.length = length;
+  newShip.damage = [];
+
+  for (let i = 0; i < length; i += 1) {
+    // 0 signifies not yet hit
+    newShip.damage.push(0);
+  }
+
+  return newShip;
+}
+
+module.exports = Ship;
