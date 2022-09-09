c93950eef9aff3169c87acc051249013d3648ee7
Josh S <aaabccc87@protonmail.com>
2022-09-09 14:27:58 -0400

    Create player to hold a gameboard and have an enemy to attack

---
+++ player.js
@@ -0,0 +1,35 @@
+const Gameboard = require('./gameboard');
+
+const playerProto = {
+  setEnemy(enemy) {
+    this.enemy = enemy;
+  },
+  getEnemy() {
+    return this.enemy;
+  },
+  attack(row, col) {
+    const attackedSquare = this.enemy.gameboard.receiveAttack(row, col);
+    if (attackedSquare !== null) {
+      attackedSquare.ship.hit(attackedSquare.ship.piece);
+      return true;
+    }
+
+    return false;
+  },
+  attackRandom() {
+    const row = Math.floor(Math.random() * 10);
+    const col = Math.floor(Math.random() * 10);
+
+    return this.attack(row, col);
+  },
+}
+function Player(playerName) {
+  const newPlayer = Object.create(playerProto);
+
+  newPlayer.playerName = playerName;
+  newPlayer.gameboard = Gameboard();
+
+  return newPlayer;
+}
+
+module.exports = Player;
